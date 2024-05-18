a046497cdf45f0e8e8b6c664153afb3c51d5d869
Galiulina Oksana <oksana17.06@bk.ru>
2024-05-18 06:42:03 +0000

    a condition for deletion has been added

--- script.sh
+++ script.sh
@@ -8,4 +8,18 @@ read filename
 if [ -f "$parent_dir/$filename" ]; then
     echo "Файл $filename существует. Выводим первые 10 строк файла:"
     head "$parent_dir/$filename"
+
+    echo "Хотите удалить файл? (да/нет)"
+    read answer
+
+    if [ "$answer" = "да" ]; then
+        
+        echo "Файл $filename был удален"
+
+    elif [ "$answer" = "нет" ]; then
+        echo "Файл не был удален"
+
+    else
+        echo "Команда не распознана"
+    fi
 fi
