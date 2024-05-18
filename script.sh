750a561079608059350ca60e9e165f2c742a2582
Galiulina Oksana <oksana17.06@bk.ru>
2024-05-18 06:11:20 +0000

    taking the parent directory and outputting the file has been added

--- script.sh
+++ script.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+parent_dir=$(dirname "$PWD")
+
 echo "Введите имя файла:"
 read filename
+
+if [ -f "$parent_dir/$filename" ]; then
+    echo "Файл $filename существует. Выводим первые 10 строк файла:"
+    head "$parent_dir/$filename"
+fi
